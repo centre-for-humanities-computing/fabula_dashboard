c0d9177281d1d2cbe7852b02919823da08068176
Niels Aalund Krogsgaard <au619572@d56760.eduroam.net.au.dk>
2024-02-26 16:40:51 +0100

    created functioning dash script (upload.py). Dropdown, upload, summary table and barplot implemented

---
+++ src/mean_calculations.py
@@ -0,0 +1,17 @@
+# script for reading in .xlsx files and calculating the mean of the columns with numerical data
+
+# read in the file
+import pandas as pd
+df = pd.read_excel('data/CHICAGO_MEASURES_FEB24.xlsx')
+
+# calculate the mean of the columns with numerical data
+# identify the columns with numerical data
+numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+# calculate the mean of the columns with numerical data
+mean = df[numerical_columns].mean()
+# save the mean to a new dataframe with the old column names and one row wth the mean
+mean_df = pd.DataFrame(mean, columns=['Mean'])
+# transpose the dataframe
+mean_df = mean_df.T
+# save the mean dataframe to a csv file
+mean_df.to_csv('data/mean.csv')
---
+++ src/upload.py
@@ -0,0 +1,116 @@
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
+
+import base64
+import datetime
+import io
+
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# read in mean file
+mean_df = pd.read_csv('data/mean.csv')
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    children = [
+        html.H1(children='Language in Text'),
+        dcc.Dropdown(['English', 'Danish'], id='lang-dropdown', placeholder="Select a lagnguage"),
+        html.H1(children='Sentiment Analysis to use'),
+        dcc.Dropdown(['afinn', 'vader', 'syuzhet', 'avg_syuzhet_vader'], id='sent-dropdown', placeholder="Select sentiment analysis method"),
+        dcc.Upload(id='upload-data', children=html.Div(['Drag and Drop or ', html.A('Select Files')]), style={'width': '100%','height': '120px', 'lineHeight': '120px','borderWidth': '1px','borderStyle': 'dashed','borderRadius': '5px','textAlign': 'center','margin': '10px'},multiple=True),
+        html.Div(id='output-data-upload'),
+    ]
+)
+
+# app.layout = html.Div([
+#     dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown'),
+#     html.Div(id='dd-output-container')
+# ])
+
+def parse_contents(contents, filename, date, language, sentiment):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'txt' in filename:
+            # Assume that the user uploaded a CSV file
+            full_string = decoded.decode('utf-8')
+        # elif 'xls' in filename:
+        #     # Assume that the user uploaded an excel file
+        #     df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    
+    # create pandas dataframe that calculates string length and number of white spaces of string
+    # only use first 20 characters of the string
+    df = pd.DataFrame({'String': full_string[:20]}, index = [0])
+    df['Name'] = filename
+    df['Length'] = df['String'].apply(len)
+    df['WhiteSpaces'] = df['String'].apply(lambda x: len(x) - len(x.replace(' ', '')))
+    df['Language'] = language
+
+    # sentiment analysis
+    if sentiment == 'afinn':
+        #df['Sentiment'] = df['String'].apply(lambda x: afinn.score(x))
+        df['Sentiment'] = sentiment
+    elif sentiment == 'vader':
+        #df['Sentiment'] = df['String'].apply(lambda x: vader.polarity_scores(x))
+        df['Sentiment'] = sentiment
+    elif sentiment == 'syuzhet':
+        #df['Sentiment'] = df['String'].apply(lambda x: syuzhet.get_sentiment(x))
+        df['Sentiment'] = sentiment
+    elif sentiment == 'avg_syuzhet_vader':
+        #df['Sentiment'] = df['String'].apply(lambda x: (syuzhet.get_sentiment(x) + vader.polarity_scores(x)['compound'])/2)
+        df['Sentiment'] = sentiment
+    
+
+
+    return html.Div([
+        #html.H5(filename),
+        #html.H6(datetime.datetime.fromtimestamp(date)),
+
+        html.Hr(),  # horizontal line
+
+        html.P(children=(full_string)),
+
+        html.Hr(),  # horizontal line
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        dcc.Graph(
+            figure={
+                "data": [
+                    {"y": df["Length"], "type": "bar", "name": filename},
+                    {"y": mean_df["TITLE_LENGTH"], "type": "bar", "name": filename},
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+    ])
+
+@callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'),
+              Input('lang-dropdown', 'value'),
+              Input('sent-dropdown', 'value'))
+def update_output(list_of_contents, list_of_names, list_of_dates, language, sentiment):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d, language, sentiment) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+
+        return children
+
+if __name__ == '__main__':
+    app.run(debug=True)
