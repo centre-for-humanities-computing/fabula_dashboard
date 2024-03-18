b3c8022325a12198a3dc3bc11616b6ae10b9045f
nielsaak <nielskrogsgaard96@gmail.com>
2024-03-18 10:08:26 +0100

    Made metrics_calculation work and added submit button

--- src/upload.py
+++ src/upload.py
@@ -25,8 +25,9 @@ app.layout = html.Div(
         dcc.Dropdown(options={'english': 'English', 'danish': 'Danish'}, id='lang-dropdown', placeholder="Select a lagnguage"),
         html.H1(children='Sentiment Analysis to use'),
         dcc.Dropdown(['afinn', 'vader', 'syuzhet', 'avg_syuzhet_vader'], id='sent-dropdown', placeholder="Select sentiment analysis method"),
-        dcc.Upload(id='upload-data', children=html.Div(['Drag and Drop or ', html.A('Select Files')]), style={'width': '100%','height': '120px', 'lineHeight': '120px','borderWidth': '1px','borderStyle': 'dashed','borderRadius': '5px','textAlign': 'center','margin': '10px'},multiple=True),
-        dcc.Textarea(id='textarea-example',value='Textarea content initialized\nwith multiple lines of text',style={'width': '100%', 'height': 100}),
+        html.Div([html.Div(dcc.Upload(id='upload-data', children=html.Div(['Drag and Drop or ', html.A('Select Files')]), style={'width': '100%','height': '120px', 'lineHeight': '120px','borderWidth': '1px','borderStyle': 'dashed','borderRadius': '5px','textAlign': 'center','margin': '10px'},multiple=True), style={'display': 'inline-block'}),
+                  html.Div(dcc.Textarea(id='textarea-example',value=None,style={'width': '100%', 'height': 100}), style={'display': 'inline-block'})]),
+        html.Button('Submit', id='submit-val', n_clicks=0),
         html.Div(id='output-data-upload'),
         html.H2(children='Explanation of Metrics'),
         html.P(children=explanation_text),
@@ -40,22 +41,26 @@ app.layout = html.Div(
 
 def parse_contents(contents, filename, date, language, sentiment, text):
     
-    # if contents == ...:
-    content_type, content_string = contents.split(',')
+    if filename is not None:
+        # if contents == ...:
+        content_type, content_string = contents.split(',')
 
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'txt' in filename:
-            # Assume that the user uploaded a CSV file
-            full_string = decoded.decode('utf-8')
-        # elif 'xls' in filename:
-        #     # Assume that the user uploaded an excel file
-        #     df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'txt' in filename:
+                # Assume that the user uploaded a CSV file
+                full_string = decoded.decode('utf-8')
+            # elif 'xls' in filename:
+            #     # Assume that the user uploaded an excel file
+            #     df = pd.read_excel(io.BytesIO(decoded))
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+        
+    if filename is None:
+        full_string = contents
         
     # elif text == ...:
     #     full_string == text
@@ -89,7 +94,15 @@ def parse_contents(contents, filename, date, language, sentiment, text):
         df['Sentiment'] = sentiment
 
     dict_0 = compute_metrics(full_string, language, sentiment)
-    df = pd.DataFrame(dict_0)
+
+    # compute metrics is producing lists in its dict, and don't know what to do with them
+    del dict_0['concreteness'] # temporary fix
+    del dict_0['valence'] # temporary fix
+    del dict_0['arousal'] # temporary fix
+    del dict_0['dominance'] # temporary fix
+    del dict_0['arc'] # temporary fix
+
+    df = pd.DataFrame(dict_0, index = [0])
 
     return html.Div([
         #html.H5(filename),
@@ -112,15 +125,15 @@ def parse_contents(contents, filename, date, language, sentiment, text):
 
         html.Hr(),  # horizontal line
 
-        dcc.Graph(
-            figure={
-                "data": [
-                    {"y": df["Length"], "type": "bar", "name": filename},
-                    {"y": mean_df["TITLE_LENGTH"], "type": "bar", "name": filename},
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
-        ),
+        # dcc.Graph(
+        #     figure={
+        #         "data": [
+        #             {"y": df["Length"], "type": "bar", "name": filename},
+        #             {"y": mean_df["TITLE_LENGTH"], "type": "bar", "name": filename},
+        #         ],
+        #         "layout": {"title": "Average Price of Avocados"},
+        #     },
+        # ),
     ])
 
 @callback(Output('output-data-upload', 'children'),
@@ -129,14 +142,22 @@ def parse_contents(contents, filename, date, language, sentiment, text):
               State('upload-data', 'last_modified'),
               Input('lang-dropdown', 'value'),
               Input('sent-dropdown', 'value'),
-              Input('textarea-example', 'value'))
-def update_output(list_of_contents, list_of_names, list_of_dates, language, sentiment, text):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d, language, sentiment, text) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+              Input('textarea-example', 'value'),
+              Input('submit-val', 'n_clicks'),
+              prevent_initial_call=True)
+def update_output(list_of_contents, list_of_names, list_of_dates, language, sentiment, text, n_clicks):
+    if n_clicks > 0:
+        if list_of_contents is not None:
+            children = [
+                parse_contents(c, n, d, language, sentiment, text) for c, n, d in
+                zip(list_of_contents, list_of_names, list_of_dates)]
 
-        return children
+            return children
+        
+        if text is not None:
+            children = parse_contents(text, list_of_names, list_of_dates, language, sentiment, text)
+
+            return children
 
 if __name__ == '__main__':
     app.run(debug=True)
